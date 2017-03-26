4f6abd7ff3090da5a02ae74e011a0e028becc34d
Steven Gates <ig11@zips.uakron.ed>
2017-03-26 03:35:50 +0000

    Added comments.

--- ats.py
+++ ats.py
@@ -7,12 +7,7 @@ class ATS:
     def __init__(self):
         self.key = '81UZVRVH3uldQNobChyi65jPLQbFux3f'
 
-    # Sample get request, to get all jobs
-    # url = 'https://api.resumatorapi.com/v1/jobs?apikey=' + key
-    # content = json.loads(urllib2.urlopen(url).read())
-    # print(content)
-
-    # Get all answers to the questionnaire
+    # get all answers to the questionnaire
     def get_response(self, url):
         content = json.loads(urllib2.urlopen(url + self.key).read())
         if not content:
@@ -20,17 +15,19 @@ class ATS:
             return None
         return content
 
+    # get names of a particular applicant
     def get_names(self, applicant_id):
         url = 'https://api.resumatorapi.com/v1/applicants/' + applicant_id + '?' + 'apikey='
         response = self.get_response(url)
         return response['first_name'], response['last_name']
 
+    # get answers for a particular questionnaire, of a particular application
     def get_answers(self, applicant_id):
         url = 'https://api.resumatorapi.com/v1/questionnaire_answers/questionnaire_id/questionnaire_20170325222929_1MD9CIFKLEZ58ZAX/applicant_id/' + applicant_id + '?apikey='
         response = self.get_response(url)
-        # answers['answers'] = response
         return response
 
+    # main function that should be used to get all data (all information for all applicants)
     def get_all(self):
         url = 'https://api.resumatorapi.com/v1/jobs/job_20170322142227_DYY2JLPRNHJ2TKRE?apikey='
         response = self.get_response(url)
